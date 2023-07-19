18f41e0cac47c0d553184b009aacc8897152c59c
Victor Valar <valar@victorvalar.me>
2023-07-19 00:55:33 -0300

    update push on aws_deploy.sh

--- aws_deploy.sh
+++ aws_deploy.sh
@@ -7,8 +7,8 @@ docker -t "$DOCKER_IMAGE_NAME" .
 echo 'Tagging image...'
 docker tag "$DOCKER_IMAGE_NAME" "$DOCKER_REGISTRY/$DOCKER_IMAGE_NAME"
 
-echo "Pushing image to Docker registry..."
-docker push "$DOCKER_REGISTRY/$DOCKER_IMAGE_NAME"
+echo "Pushing image to AWS ECR..."
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${DOCKER_TAG}
 
 echo "Updating lambda function..."
 aws lambda update-function-code --function-name "$FUNCTION_NAME" --image-uri "$DOCKER_REGISTRY/$DOCKER_IMAGE_NAME:latest"
